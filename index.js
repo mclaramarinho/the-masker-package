8205f365c9be9eead659e87fbb1d51f73bee400c
mclaramarinho <marinho.claramb@gmail.com>
2024-06-20 21:25:11 -0300

    FEAT: created index.js with the event listener and module exports

---
+++ index.js
@@ -0,0 +1,23 @@
+import { maskValue } from "./lib/masker.js";
+function applyMask(){
+    const allMaskedInputs = document.querySelectorAll(".masked");
+
+    allMaskedInputs.forEach(el => {
+        el.addEventListener("keyup", ev => {
+            const mask = el.getAttribute("mask");
+            const value = el.value;
+
+            if(ev.key!=="Backspace"){
+                const newValue = maskValue(mask, value);
+                
+                el.value = newValue;
+                el.setAttribute('value', newValue);
+            }
+
+        })
+    })
+}
+
+module.exports = {
+    applyMask,
+}
