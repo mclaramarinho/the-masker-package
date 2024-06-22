b08594b3c955e64a18c372c5a0cde61090d3c950
mclaramarinho <marinho.claramb@gmail.com>
2024-06-22 10:59:17 -0300

    FIX: [Issue#1] - refined event listener selector

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import { maskValue } from "./lib/masker.js";
 function applyMask(){
-    const allMaskedInputs = document.querySelectorAll(".masked");
+    const allMaskedInputs = document.querySelectorAll('input[type="text"].masked');
 
     allMaskedInputs.forEach(el => {
         el.addEventListener("keyup", ev => {
             const mask = el.getAttribute("mask");
+            if(mask === null) return console.warn("Mask attribute is required if you add the \'masked\' class");
+            if(mask.length === 0) return console.warn("Mask attribute can't be empty");
+            
+
             const value = el.value;
 
             if(ev.key!=="Backspace"){
                 const newValue = maskValue(mask, value);
-                
+
                 el.value = newValue;
                 el.setAttribute('value', newValue);
             }
@@ -18,4 +22,6 @@ function applyMask(){
     })
 }
 
-exports.applyMask = applyMask
+// exports.applyMask = applyMask]
+
+applyMask();
